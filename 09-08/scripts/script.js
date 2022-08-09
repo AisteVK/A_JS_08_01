f7a84f8a42f7ee9dab3fe4d32363ed3499be5766
AisteVK <vaivadaite@gmail.com>
2022-08-09 23:55:10 +0300

    Counting Page

---
+++ 09-08/scripts/script.js
@@ -0,0 +1,45 @@
+function addition(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = document.getElementById("result");
+    if (!Number(num1) || !Number(num2)){
+        result.innerText = "Number not provided";}
+    else {
+        let resultSum = Number(num1) + Number(num2) 
+        result.innerText = resultSum;}
+}
+
+function subtraction(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = document.getElementById("result");
+    if (!Number(num1) || !Number(num2)){
+        result.innerText = "Number not provided";}
+    else {
+        let resultMinus = Number(num1) - Number(num2);
+        result.innerText = resultMinus;}
+}
+
+function multiplication(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = document.getElementById("result");
+    if (!Number(num1) || !Number(num2)){
+        result.innerText ="Number not provided";}
+    else {
+        let resultMulti = Number(num1) * Number(num2);
+        result.innerText = resultMulti;}
+}
+
+function division(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const result = document.getElementById("result");
+    if (!Number(num1) || !Number(num2)){
+        result.innerText = "Number not provided";}
+    else {
+        let resultDiv = Number(num1) / Number(num2);
+        result.innerText = resultDiv;
+        }
+    
+}
