0adff6c556eca228ff003475f8fb9f699afcba65
AisteVK <vaivadaite@gmail.com>
2022-08-16 17:08:50 +0300

    table tasks

--- 09_08/scripts/script.js
+++ 09_08/scripts/script.js
@@ -43,3 +43,10 @@ function division(){
         }
     
 }
+
+function clearInputs(){
+    const inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++){
+        inputs[i].value = "";
+    }
+}
---
+++ 11_08/scripts/script.js
@@ -0,0 +1,30 @@
+function highlightEntry(){
+    let value = $("#colorOperation").val();
+    if(value == "Select"){
+        $(".col1").show();
+    }else{
+        $(".col1").hide();
+        $(".col2").hide();
+    }
+    }
+
+function addColor(){
+    const container = document.getElementsByClassName("container")[0];
+    const element1 = document.createElement("div");
+    const input1 = document.getElementById("input1").value;
+    element1.textContent = input1;
+    container.appendChild(element1);
+}
+
+/*
+function addCountry(){
+    const container = document.getElementsByClassName("container")[0];
+    const element2 = document.createElement("div");
+    const input2 = document.getElementById("input2").value;
+    element2.textContent = input2;
+    container.appendChild(element2);
+}
+
+*/ 
+
+// ... notepad
